9f2230c3a304c86922fde34b72b217ffda00ca3d
Mike MacCana <mike.maccana@gmail.com>
2012-06-27 19:11:14 +0100

    Swap from location hack to adding script tags. Use 2 space indents.

--- content.js
+++ content.js
@@ -1,13 +1,25 @@
 // In order to send keyboard events, we'll need to send them from the page's JS
-var load_into_page_context = function(file) {
-	var file_url = chrome.extension.getURL(file);
-	$.get(file_url, function(script_contents) {
-		window.location = 'javascript:'+script_contents
-	})
-	
+
+// Add some JS to the location bar
+var loadJSFileToLocationBar = function(file) {
+  var file_url = chrome.extension.getURL(file);
+  $.get(file_url, function(script_contents) {
+    window.location = 'javascript:'+script_contents
+  })
 }
 
-// It takes around 2 secs for the pages own JS to load the words.
-setTimeout( function() {
-	load_into_page_context("injectme.js");
-}, 2000)	
+// Add a script tag pointing to a file from our extension into the page's DOM
+// this script will execute in the page's own JS world
+var injectJSFile = function(file) {
+  var url = chrome.extension.getURL(file);    
+  $('<script type="text/javascript" src="'+url+'"/>').appendTo($('head')); 
+}
+
+$(function(){
+  console.log('DOM ready, about to inject...')
+  // It takes around 2 secs for the pages own JS to load the words.
+  setTimeout( function() {
+    //loadJSFileToLocationBar("injectme.js");
+    injectJSFile("injectme.js");
+  }, 2000)    
+})
--- injectme.js
+++ injectme.js
@@ -4,12 +4,14 @@ var SPACE_KEYCODE = 32;
 var space_down = $.Event( 'keyup', { which: SPACE_KEYCODE } );
 var words = [];
 $('div#words > div#row1').children().each(function(index, element) { 
-	words.push( $(element).text() );
+  words.push( $(element).text() );
 });
 var count = 0;
 var interval = setInterval( function() { 
-		$inputbox.val(words[count]+' ');
-		$inputbox.trigger(space_down);
-		count++; 
-		if ( count >= words.length) clearInterval(interval);
-    }, 200);
+  $inputbox.val(words[count]+' ');
+  $inputbox.trigger(space_down);
+  count++; 
+  if ( count >= words.length) {
+    clearInterval(interval)
+  };
+}, 150);
