d85bca357dda81c6acce9089142428c31ebd09c6
Mike MacCana <mike.maccana@gmail.com>
2012-06-27 20:56:08 +0100

    Remove unused location-bar inject code, add comments, whitespace fixes.

--- content.js
+++ content.js
@@ -1,13 +1,5 @@
 // In order to send keyboard events, we'll need to send them from the page's JS
 
-// Add some JS to the location bar
-var loadJSFileToLocationBar = function(file) {
-  var file_url = chrome.extension.getURL(file);
-  $.get(file_url, function(script_contents) {
-    window.location = 'javascript:'+script_contents
-  })
-}
-
 // Add a script tag pointing to a file from our extension into the page's DOM
 // this script will execute in the page's own JS world
 var injectJSFile = function(file) {
@@ -19,7 +11,6 @@ $(function(){
   console.log('DOM ready, about to inject...')
   // It takes around 2 secs for the pages own JS to load the words.
   setTimeout( function() {
-    //loadJSFileToLocationBar("injectme.js");
     injectJSFile("injectme.js");
   }, 2000)    
 })
--- injectme.js
+++ injectme.js
@@ -2,10 +2,14 @@ console.log('Completing the test');
 var $inputbox = $('input#inputfield');
 var SPACE_KEYCODE = 32;
 var space_down = $.Event( 'keyup', { which: SPACE_KEYCODE } );
+
+// There's a hidden div with our words. Let's get 'em.
 var words = [];
 $('div#words > div#row1').children().each(function(index, element) { 
   words.push( $(element).text() );
 });
+
+// Simulate the required user input
 var count = 0;
 var interval = setInterval( function() { 
   $inputbox.val(words[count]+' ');
